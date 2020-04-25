e8257b410f8dc584bf38881c7d72902ee4cc9579
jeong3bin <jn8121@naver.com>
2020-04-25 23:58:35 +0900

    Updates

---
+++ precache-manifest.68401644ba87215ff3cf29909ccb29eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d45bb2f951e3b4eae7087ed34f1317f",
+    "url": "/movie_app_test/index.html"
+  },
+  {
+    "revision": "266883b02d4ca5c3798a",
+    "url": "/movie_app_test/static/css/main.56bac88a.chunk.css"
+  },
+  {
+    "revision": "0987e066dfe4f7e43fd6",
+    "url": "/movie_app_test/static/js/2.e48d6fbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_test/static/js/2.e48d6fbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266883b02d4ca5c3798a",
+    "url": "/movie_app_test/static/js/main.14ed9912.chunk.js"
+  },
+  {
+    "revision": "0578ad059e6da4673c11",
+    "url": "/movie_app_test/static/js/runtime-main.ffd9a3e5.js"
+  }
+]);
